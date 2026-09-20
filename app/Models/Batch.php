017ce8701b8c9da80ea200efc4af9bed82fc3f3d
DukeVin <astralguard14@gmail.com>
2026-09-20 10:47:24 +0700

    feat(batch): add kapasitas support and helper methods to Batch model

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -23,6 +23,7 @@ class Batch extends Model
         'tanggal_masuk' => 'date',
         'tanggal_kadaluarsa' => 'date',
         'jumlah_awal' => 'integer',
+        'kapasitas' => 'integer',
         'stok_saat_ini' => 'integer',
     ];
 
@@ -70,10 +71,81 @@ class Batch extends Model
     {
         return $this->stok_saat_ini < $this->kapasitas;
     }
-    
+
     // Sisa kapasitas
     public function sisaKapasitas(): int
     {
         return $this->kapasitas - $this->stok_saat_ini;
+    }
+
+    public function kosong(): bool
+    {
+        return $this->stok_saat_ini <= 0;
+    }
+
+    /**
+     * Cek apakah batch penuh
+     */
+    public function penuh(): bool
+    {
+        return $this->stok_saat_ini >= $this->kapasitas;
+    }
+
+    /**
+     * Tambah isi batch (dengan validasi kapasitas)
+     * 
+     * @param int $jumlah
+     * @return bool
+     * @throws \Exception
+     */
+    public function tambahIsi(int $jumlah): bool
+    {
+        if ($jumlah <= 0) {
+            throw new \Exception('Jumlah harus lebih dari 0');
+        }
+
+        if ($this->stok_saat_ini + $jumlah > $this->kapasitas) {
+            throw new \Exception(
+                'Melebihi kapasitas batch! Sisa kapasitas: ' . $this->sisaKapasitas()
+            );
+        }
+
+        $this->stok_saat_ini += $jumlah;
+        $this->save();
+
+        return true;
+    }
+
+    /**
+     * Kurangi isi batch (dengan validasi stok)
+     * 
+     * @param int $jumlah
+     * @return bool
+     * @throws \Exception
+     */
+    public function kurangiIsi(int $jumlah): bool
+    {
+        if ($jumlah <= 0) {
+            throw new \Exception('Jumlah harus lebih dari 0');
+        }
+
+        if ($this->stok_saat_ini < $jumlah) {
+            throw new \Exception(
+                'Stok batch tidak mencukupi! Stok saat ini: ' . $this->stok_saat_ini
+            );
+        }
+
+        $this->stok_saat_ini -= $jumlah;
+        $this->save();
+
+        return true;
+    }
+
+    /**
+     * Format tampilan stok
+     */
+    public function getStokFormatAttribute(): string
+    {
+        return $this->stok_saat_ini . '/' . $this->kapasitas;
     }
 }
